1e36aba764d0d538163ef36a77dd0c16b5310b33
brijeshdubey8779 <brijeshdubey8451@gmail.com>
2025-01-10 16:54:54 +0530

    Dockerfile and Docker-compose.yml added and api routes added

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,13 +1,70 @@
 from flask import Blueprint, jsonify, current_app, request
 from app import mongo
+from werkzeug.security import generate_password_hash, check_password_hash
 
 main = Blueprint('main', __name__)
 
 @main.route('/api/data', methods=['GET','POST'])
 def get_data():
-    # mongo = current_app.extensions['mongo']
+    
     data = mongo.db.users.find_one()
     if data:
         data['_id'] = str(data['_id'])
         return jsonify(data)
     return jsonify({"message": "Hello , World!"})
+
+@main.route('/api/register', methods=['POST'])
+def register():
+        
+    if request.method == "POST":
+        data = request.get_json()
+        try:
+            if data.get('email') == None:
+                return jsonify({"message": "Email is required"}), 400
+            
+            if data.get('password') == None:
+                return jsonify({"message": "Email is required"}), 400
+            
+            if mongo.db.users.find_one({"email": data['email']}):
+                return jsonify({"message": "Email already exists"})
+            data['password'] = generate_password_hash(data.get('password'), method='scrypt')
+            mongo.db.users.insert_one(data)
+            return jsonify({"message": "Data added successfully!"}), 200
+        except Exception as e:
+            return jsonify({"message": e})
+        
+    
+
+@main.route("/api/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    if data.get('email') == None:
+        return jsonify({"message": "Email is required"}), 400
+    
+    if data.get('password') == None:
+        return jsonify({"message": "Password is required"}), 400
+    
+    if not mongo.db.users.find_one({"email": data['email']}) or check_password_hash(data['password'], mongo.db.users.find_one({"password": data['password']})):
+        return jsonify({"message": "please Check your emial or password"}), 400
+    else:
+        return jsonify({"message": "Logged In"}), 200
+    # return jsonify({"message": "Data added successfully!"})
+    
+@main.route('/api/delete', methods=['POST'])
+def delete_data():
+    
+    mongo.db.users.delete_many({})
+    return jsonify({"message": "Data deleted successfully!"})
+            
+
+@main.route("/api/update", methods=["POST"])
+def update_data():
+    try:
+        mongo.db.users.update_many({"name": "Anya"}, {"$set": {"age": 22}})
+    except Exception as e:
+        print(e)
+        return jsonify({"message": e})
+    
+    return jsonify({"message": "Data updated successfully!"})
+
+            
